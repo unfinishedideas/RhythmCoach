75234a26330416f62234e8dc452b385c3b5afdf5
Student <example@example.com>
2020-03-02 16:07:02 -0800

    basic rhythms and loops going

--- src/components/ToneTest.js
+++ src/components/ToneTest.js
@@ -18,40 +18,68 @@ function ToneTest() {
         }
     };
 
-
-
     // Load Samples
     let kickBuffer = new Tone.Buffer(kickSample);
     let snareBuffer = new Tone.Buffer(snareSample);
     let hatBuffer = new Tone.Buffer(hatSample);
 
-    let kickPlayer = new Tone.Player(kickBuffer, function () { console.log('kick sample initialized'); }).toMaster();    
+    let kickPlayer = new Tone.Player(kickBuffer, function () { console.log('kick sample initialized'); }).toMaster();
     let snarePlayer = new Tone.Player(snareBuffer, function () { console.log('snare sample initialized'); }).toMaster();
     let hatPlayer = new Tone.Player(hatBuffer, function () { console.log('hat sample initialized'); }).toMaster();
-    
-    
+
+
     // Playback ========================================================================================================
     Tone.Buffer.on('load',
         function onLoad(){
             console.log('All samples loaded');
-            setListen(false);
+            setListen(true);
+           
+            Tone.Transport.bpm.value = 100;
+            Tone.Transport.loopStart = 0;
+            Tone.Transport.loopEnd = '4m'
+            Tone.Transport.loop = true;
             Tone.Transport.start();
-            let looper = new Tone.Loop(song, '4n');
+           
+            let looper = new Tone.Loop(song, '16n');
             looper.start(0);
         },
         function onError(){
             console.log("Something didn't load properly");
         }
     );
-    
-    let counter = 1;
+
     let currentBeatTick = 0;
+    let counter = 0;
+    // Rhythm Key
+    // 1  e  +  a     2  e  +  a     3  e  +  a     4  e  +  a  
+    // 1  2  3  4     5  6  7  8     9  10 11 12    13 14 15 16
+    
     function song(time) {
         // console.log(Tone.Transport.ticks);
         // console.log(counter);
-        kickPlayer.start();
-        currentBeatTick = Tone.Transport.getTicksAtTime();
+
+        // Snares
+        if (counter === 5 || counter === 13){
+            snarePlayer.start();
+            currentBeatTick = Tone.Transport.getTicksAtTime();
+        }
+        // Hats
+        if (counter %2 !== 0) {
+            hatPlayer.start();
+        }
+        // Kicks
+        const kickRhythmArray = [1,3,0,9,10,12];
+        if (kickRhythmArray.includes(counter) ) {
+            kickPlayer.start();
+        }
+        if (counter < 16) {
+            counter += 1
+        }
+        else {
+            counter = 1;
+        }
     }
+
     function displayTime(string) {
         console.log(`time of ${string}:`, Tone.Transport.getTicksAtTime());
     }
@@ -75,7 +103,8 @@ function ToneTest() {
             console.log('starting');
             Tone.Transport.start();
             transportOn = true;
-        }        
+            counter = 1;
+        }
     }
 
     // Game stuff ======================================================================================================
@@ -84,7 +113,7 @@ function ToneTest() {
         console.log('target: ', currentBeatTick);
         console.log('input:', inputTick);
         console.log('difference: ', inputTick - currentBeatTick);
-        
+
     }
 
 
@@ -96,6 +125,7 @@ function ToneTest() {
             <button onClick={toggleTransport}>Start/Stop</button>
             <br/>
             <button onClick={() => setListen(true)}>Fastest Latency</button>
+            <button onClick={() => playSnare()}>CLICK</button>
             <br/>
             <button onClick={() => setListen(false)}>Playback Latency (stops sound)</button>
         </div>
